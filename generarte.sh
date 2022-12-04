f49ddfcdb9e237bb6617ad1c7de43f85f5530773
FacilitadorTIC <69331016+FacilitadorTIC@users.noreply.github.com>
2022-12-03 23:50:16 -0300

    Create generarte.sh

---
+++ generarte.sh
@@ -0,0 +1,20 @@
+# GenerArte
+# Generador de ISO personalizada
+# Contiene archivos en carpetas que serán llamadas por el "generador".
+#
+# Definimos las variables
+#
+VER=1.0.1
+BASE=bullseye
+ARCH=amd64
+ROOT=rootdir
+FILE=setup.sh
+USER=TIC
+NONFREE=true
+#
+# Inicializar colores del texto
+rojo='\e[1;31m'
+blanco='\e[1;37m'
+amarillo='\e[1;33m'
+apagado='\e[0m'
+
